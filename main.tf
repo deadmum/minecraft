49d54c86840bebc17c80be042a3f55b9b55a1aba
deadmum <janik.knodel@gmail.com>
2023-11-16 11:50:12 +0100

    main.tf updates mit Public Key, fester Region

--- main.tf
+++ main.tf
@@ -7,20 +7,20 @@ terraform {
   }
 }
 
-variable "your_region" {
-  type        = string
-  description = "eu-central-1"
-}
+#variable "your_region" {
+#  type        = string
+#  description = "eu-central-1"
+#}
 
 variable "your_ip" {
   type        = string
   description = "insert your IP e.g. https://www.whatsmyip.org/"
 }
 
-variable "your_public_key" {
-  type        = string
-  description = "insert SSH Token"
-}
+#variable "your_public_key" {
+#  type        = string
+#  description = "insert SSH Token"
+#}
 
 variable "mojang_server_url" {
   type        = string
@@ -32,7 +32,7 @@ variable "mojang_server_url" {
 
 provider "aws" {
   profile = "default"
-  region  = var.your_region
+  region  = "eu-central-1"
 }
 
 resource "aws_security_group" "minecraft" {
@@ -64,7 +64,7 @@ resource "aws_security_group" "minecraft" {
 
 resource "aws_key_pair" "home" {
   key_name   = "Home"
-  public_key = var.your_public_key
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCoJXZkOgs6U0JvRQOUbxsqgYjHwlv4vqnzh8RyACnLq/p8nO8WzGlkNCbGJ9UkIpHkxPaKC8W/tctNIc42OC4jGgrHfJrM/pZC4M8QyRznZfiummTcRyUoLaJJMKoZxDc0hrIm19h12riYndyOsvIZFpCxJ9RRNDKi38zrYEtm1ZILpyyR8KX92+PP8Zxj7uHEL1k0ZFiErhKSI2Wk0D967o3yOlYExvZVDCSL8xth5H5rrcEmjJHmnAt2tWwIj7lV5Q0AfHD2cX3fRBerbtAvBWv7jhGFk6H7H4aol56VWl/9c4T4Fk9cTCCsmNPDiTvTGJ4gV9SX3LKaob82TLRTeEGdAjcDsyiNBxS+t3vgtMbwwExFfc6YYLMqEIydJhuf5W0gduxa8UwawItGMR0ykyPPmHstraM+R5HAFkKGiW0VEHDkpylQ4kfctVSMZyN4Ov0TXEpepQaGmdftIYxjKn2hX/7SD4UPzusVRW1ZjPYW5/hcOQ5Zi1wrrND2BDkDSPKl1qJ0Cydz7p5NjrFAopp2kKK6JsiGJ3MeF350pjSrUxYKH326ahzedtcf6jmz2KM6jieRPnX6cQimYnf/NaxQhzCR55J8LTw0La8BB8/2y7frESq7vz8carKewpjQKZbHt1b56R/9kAq8ixQ1ythIq0nJLOoQlJR8z5DMmw== janik.knodel@gmail.com"
 }
 
 resource "aws_instance" "minecraft" {
